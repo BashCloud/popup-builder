d15d704f615671c49104a13253f37c5b61fe9af9
Pulkit <pulkit.cloud@gmail.com>
2022-09-05 12:59:49 +0530

    ADDED: Preview Page

---
+++ src/views/Preview.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Preview Page</div>
+</template>
+
+<script>
+export default {
+  created() {
+    let headEl = document.head || document.getElementsByTagName('head')[0];
+    let scriptNode = document.createElement('script');
+    scriptNode.src = 'http://localhost:3000/pixel.js';
+    scriptNode.async = true;
+    headEl.appendChild(scriptNode);
+  },
+};
+</script>
